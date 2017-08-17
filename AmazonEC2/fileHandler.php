3909e43be62d4798e33733b2d9acd2920c914b5b
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 14:22:04 -0600

    Testing New Build

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -1,1 +1,13 @@
 <?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
+$uploadOk = 1;
+$textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+if(isset($_POST["submit"])) {
+    $check = new SplFileInfo($_FILES["uploadedFile"]["tmp_name"]);
+    
+    var_dump($check->getExtension());
+    
+}
+?>
--- AmazonEC2/fileInputHTML.php
+++ AmazonEC2/fileInputHTML.php
@@ -1,9 +1,9 @@
 <html>
 	<body>
 			Input File:
-		<form action = "fileHandler.php" method = "post">
+		<form action="fileHandler.php" method="post" enctype="multipart/form-data">
 			<br>
-			<input type="file" name="uploadedFile">
+			<input type="file" name="uploadedFile" id="uploadedFile">
 			<br>
 			<input type ="submit" name="submitStatus" value="Submit">
 		</form>
