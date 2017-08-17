c51e44162d6ad268bbe673028ce20c1c558b6f3a
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 14:51:31 -0600

    Added stuff

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -1,13 +1,20 @@
 <?php
+$files = glob("uploads/");
+foreach($files as $file){
+    if(is_file($file))
+       unlink($file);
+}
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
 $uploadOk = 1;
 $textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-    if($textFileType == "docx" || $textFileType == "doc" || $textFileType == "txt"){
-        echo "Valid File Type";
-    }else{
-        echo "File Type Is Invalid";
+//Is it a valid file type? -------------------
+    if($textFileType != "docx" || $textFileType != "doc" || $textFileType != "txt"){
+        echo "File Type Is Invalid - Valid Types Are: .docx, .doc, or .txt";
+        $uploadOk = 0;
     }
+    else{echo "file is valid";}
     
 ?>
