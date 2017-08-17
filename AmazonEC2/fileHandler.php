7ae330e46e90081000dae61f9e21a422e289ecdc
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 15:19:17 -0600

    Merged HTML and PHP

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -1,3 +1,15 @@
+<html>
+	<body>
+			Input File:
+		<form action="fileHandler.php" method="post" enctype="multipart/form-data">
+			<br>
+			<input type="file" name="uploadedFile" id="uploadedFile">
+			<br>
+			<input type ="submit" name="submitStatus" value="Submit">
+		</form>
+	</body>
+</html>
+
 <?php
 $files = glob("uploads/");
 foreach($files as $file){
