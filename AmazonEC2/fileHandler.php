e1360e9cf38d5bd641c89e16da1ab519173e8307
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 14:25:34 -0600

    1

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -4,6 +4,7 @@ $target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
 $uploadOk = 1;
 $textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
+echo "Works";
 if(isset($_POST["submit"])) {
     $check = new SplFileInfo($_FILES["uploadedFile"]["tmp_name"]);
     
