e48045410b5ddc50e4f8aa47a3ce7aa2405b7639
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 14:00:06 -0600

    Added Submit and Handlers

--- AmazonEC2/fileInputHTML.php
+++ AmazonEC2/fileInputHTML.php
@@ -1,9 +1,11 @@
 <html>
 	<body>
-		<form>
-		
+			Input File:
+		<form action = "fileHandler.php" method = "post">
+			<br>
 			<input type="file" name="uploadedFile">
-		
+			<br>
+			<input type ="submit" name="submitStatus" value="Submit">
 		</form>
 	</body>
 </html>
