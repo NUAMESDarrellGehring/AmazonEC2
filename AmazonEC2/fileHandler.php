9dde7616a5e4e3022b1b414c4677fafdbedb149d
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 15:10:49 -0600

    Break Lines

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -13,18 +13,22 @@ $textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 //Is it a valid file type? -------------------
     if($textFileType != "docx" && $textFileType != "doc" && $textFileType != "txt"){
         echo "File Type Is Invalid - Valid Types Are: .docx, .doc, or .txt";
+        echo "<br>";
         $uploadOk = 0;
     }
     
     if($_FILES["uploadedFile"]["size"] > 600000){
-        echo "Sorry, Your File Is Too Large: Max Size Is 600 KB";
+        echo "File Is Too Large: Max Size Is 600 KB";
+        echo "<br>";
         $uploadOk = 0;
     }
     
     if($uploadOk == 0){
         echo "File Upload Failed";
+        echo "<br>";
     }else{
         echo "File Upload Successful";
+        echo "<br>";
     }
     
     
