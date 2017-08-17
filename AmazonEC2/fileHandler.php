2af3afb7e350b582b1026877d2b7ab5b029d40ac
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 14:25:02 -0600

    Redid

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -4,9 +4,12 @@ $target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
 $uploadOk = 1;
 $textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
-
+if(isset($_POST["submit"])) {
     $check = new SplFileInfo($_FILES["uploadedFile"]["tmp_name"]);
     
-    echo var_dump($check->getExtension());
+    var_dump($check->getExtension());
     
+}
+
+echo isset($_POST["submit"]);
 ?>
