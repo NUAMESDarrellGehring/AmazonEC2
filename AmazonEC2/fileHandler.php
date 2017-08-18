1c82682aa65c8f36ab280879c821d50e0ac52934
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-17 20:22:23 -0600

    Removed HTML/Javascript Console.log on connection to mySQL

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -57,7 +57,4 @@ if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo ("<html><script> console.log(\"Connected to mySQL successfully\") </script></html>");
-
-    
 ?>
