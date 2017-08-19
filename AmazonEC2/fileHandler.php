186624ca8e587c83b3b52a2334727540bc14cd66
NUAMESDarrellGehring <majesticwaffle17@gmail.com>
2017-08-18 20:33:50 -0600

    Del

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -42,7 +42,7 @@ if($_FILES["uploadedFile"]["size"] > 0){
         echo "File Upload Failed";
         echo "<br>";
     }else{
-        if(move_uploaded_file($_FILES["uploadedFile"]["name"], $target_file)){echo("true");}else{echo("false" . );};
+        if(move_uploaded_file($_FILES["uploadedFile"], $target_file)){echo("true");}else{echo("false");};
         echo "File Upload Successful";
         echo "<br>";
     }
