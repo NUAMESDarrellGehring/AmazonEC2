8b2fc5167971635ccb415a42471c6eb32dd40d87
Darrell <Darrell@DESKTOP-93AI4FA.Home>
2017-10-21 13:43:08 -0600

    Fixed Dad's Shenanigans. Hopefully. Maybe I broke it even more.

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -1,74 +1,39 @@
 <?php
 
-function debug($str) {
-    if(isset($_REQUEST['debug']) && $_REQUEST['debug'] == "1") {
-        echo $str."\n<br>";
-    }
-}
-
-debug("Start Of Process: ".var_export($_FILES, true));
-
-if(isset($_FILES["uploadedFile"]["error"])) {
-    $phpFileUploadErrors = array(
-        0 => 'There is no error, the file uploaded with success',
-        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
-        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
-        3 => 'The uploaded file was only partially uploaded',
-        4 => 'No file was uploaded',
-        6 => 'Missing a temporary folder',
-        7 => 'Failed to write file to disk.',
-        8 => 'A PHP extension stopped the file upload.'
-    );
-    
-    //debug(var_export($phpFileUploadErrors, true));
-    //test
-    try {
-        throw new Exception("File Upload Error: ".$phpFileUploadErrors[$_FILES["uploadedFile"]["error"]]);
-    } catch(Exception $ex) {
-        $tmp_dir = ini_get('upload_tmp_dir') ? ini_get('upload_tmp_dir') : sys_get_temp_dir();
-        
-        debug("ERROR[".$_FILES["uploadedFile"]["error"]."] (".$tmp_dir.") ".$ex->getMessage());
-        exit;
-    }
-}
-
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["uploadedFile"]["name"]);
 $uploadOk = 1;
 $textFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
 //Does the file exist? ----------------------
-if($_FILES["uploadedFile"]["size"] > 0){
-    debug("Got File Size");
+    if($_FILES["uploadedFile"]["size"] == 0){
+        echo "File Does Not Exist";
+        echo "<br>";
+        $uploadOk = 0;
+    }
+    
 //Is it a valid file type? -------------------
     if($textFileType != "docx" && $textFileType != "doc" && $textFileType != "txt"){
         echo "File Type Is Invalid - Valid Types Are: .docx, .doc, or .txt";
         echo "<br>";
         $uploadOk = 0;
     }
-    
-    debug("Step 1 [".$uploadOk."]");
-    
+        
 //Is it a valid file size? -------------------
     if($_FILES["uploadedFile"]["size"] > 1024 * 700){
         echo "File Is Too Large: Max Size Is 700 KB";
         echo "<br>";
         $uploadOk = 0;
     }
-    
-    debug("Step 2 [".$uploadOk."]");
-    
+        
 //Does it check any errors? ------------------    
     if($uploadOk == 0){
         echo "File Upload Failed";
         echo "<br>";
     }
-    
-    debug("Step 3 [".$uploadOk."]");
-    
+        
     if($uploadOk !== 0) {
-        
-        debug("Uplpoad OK");
-        
+                
         $servername = "localhost";
         $username = "root";
         $password = "skull71";
@@ -112,7 +77,6 @@ if($_FILES["uploadedFile"]["size"] > 0){
         $fclose($fileForPlugin);
        echo "Test: We've reached the end of this program!";
     }
-}
 ?>
 
 <html>
