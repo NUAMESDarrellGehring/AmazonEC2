4af8fa6aac614cded65e45c8e1afd9b974cb8e86
Darrell <Darrell@DESKTOP-93AI4FA>
2017-11-02 13:20:20 -0600

    Test

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -132,8 +132,9 @@ if($_FILES["uploadedFile"]["size"] > 1024 * 700){
                     echo "This is the response: ".var_export($response_a->results[0]->geometry->location,true);
                     echo "<br>";
                     echo "End Debug Line";
+                    $plugin = var_export($response_a->results[0]->geometry->location);
                     
-                    if(isset(var_export($response_a->results[0]->geometry->location))) {
+                    if(isset($plugin)) {
                         $lat = $response_a->results[0]->geometry->location->lat;
                         $lng = $response_a->results[0]->geometry->location->lng;
                         return array($lng, $lat);
