87a978542f8568122e674ec99a3a5b14a18c7e20
Darrell Gehring <majesticwaffle17@gmail.com>
2017-12-17 19:26:33 -0700

    Mayhaps.

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -150,13 +150,13 @@ if ($connSearch->connect_error){
 if(isset($userSearch)){
     $connSearch->query("USE cityInfoDB");
     $connSearch->query("set @orig_lat=".$userCoords[1]."; set @orig_lon=".$userCoords[0]."; set @dist=".$userSearch.";");
-    $queryToPrint = $connSearch->query("SELECT *, ( 3959 * acos( cos( radians(@orig_lon) ) * cos( radians(cityInfo.latitude) ) 
+    echo $connSearch->query("SELECT *, ( 3959 * acos( cos( radians(@orig_lon) ) * cos( radians(cityInfo.latitude) ) 
                     * cos( radians(cityInfo.longitude) - radians(@orig_lat) ) + sin( radians(@orig_lat) ) * sin(radians(cityInfo.latitude)) ) ) AS distance 
                     FROM cityInfo 
                     HAVING distance < 25 
                     ORDER BY distance 
                     LIMIT 0 , 20;");
-    var_dump($queryToPrint);
+    //var_dump($queryToPrint);
     //printf($connSearch->error);
                    // echo "Done With That.";
 }
