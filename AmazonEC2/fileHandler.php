fc6c461cb6356b057e09fbb6b1911500f6a27e69
Darrell Gehring <majesticwaffle17@gmail.com>
2018-01-01 22:12:25 -0700

    GSKMDFSDF

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -150,6 +150,9 @@ if ($connSearch->connect_error){
 if(isset($userSearch)){
     $connSearch->query("USE cityInfoDB;");
     $connSearch->query("set @orig_lat=".$userCoords[1]."; set @orig_lon=".$userCoords[0]."; set @dist=".$userSearch.";");
+    $latRes = mysqli_query($connSearch, "SELECT @orig_lat;");
+    var_dump($latRes);
+    
     $searchOut = $connSearch->query("SELECT *, ( 3959 * acos( cos( radians(@orig_lat) ) * cos( radians( cityInfo.latitude ) )  * cos( radians(cityInfo.longitude) - radians(@orig_lon) ) + sin( radians(@orig_lat) ) * sin(radians(cityInfo.latitude)) ) ) AS distance  FROM cityInfo  HAVING distance < @dist  ORDER BY distance  LIMIT 0 , 20;");
                     
     // SELECT *, ( 3959 * acos( cos( radians(37) ) * cos( radians( cityInfo.latitude ) )  * cos( radians(cityInfo.longitude) - radians(-122) ) + sin( radians(37) ) * sin(radians(cityInfo.latitude)) ) ) AS distance  FROM cityInfo  HAVING distance < 25  ORDER BY distance  LIMIT 0 , 20;
