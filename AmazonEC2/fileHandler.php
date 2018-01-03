e49c6f1e0032a5da2579fa13dc3643030d2b6f13
Darrell Gehring <majesticwaffle17@gmail.com>
2018-01-02 21:17:24 -0700

    test

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -184,7 +184,7 @@ if(isset($userSearch)) {
         $cnt = 0;
         $str = "Results:<br><ul>";
         while($row = mysqli_fetch_assoc($results)) {
-            $str .= "<li>".$row['city'].": ".$row['population']."</li>";
+            $str .= "<li>".$row['city'].": ".$row['population']." <!--".var_export($row, true)."--></li>";
             $cnt++;
         }
         $str .= "</ul>";
