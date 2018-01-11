d3e60a6bdc7fe0beba083941324ec5f61de74fe6
Darrell Gehring <majesticwaffle17@gmail.com>
2018-01-11 13:43:53 -0700

    EI DAWNT

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -248,7 +248,7 @@ if(isset($userSearch)) {
     <div id="map"></div>
     <script>
       function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
+        var uluru = {lat: <?php echo json_encode($combArray[0]['longitude'], JSON_HEX_TAG); ?>, lng: <?php echo json_encode($combArray[0]['latitude'], JSON_HEX_TAG); ?>};
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: uluru
