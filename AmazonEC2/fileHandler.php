81bb1231218d57de1b0c72ac4fcadec9917d2802
Darrell Gehring <majesticwaffle17@gmail.com>
2018-01-13 18:53:15 -0700

    dffds

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -260,15 +260,9 @@ if(isset($userSearch)) {
 		<head>
 		
 		
-			<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/cupertino/jquery-ui.css">
-			<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-		
-		
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-			
-			
-			<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+			<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 			
 			
 			
