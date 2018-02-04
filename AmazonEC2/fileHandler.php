c37ae6ef5756ebc107fae014249859c3870e341f
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-04 15:48:58 -0700

    afds

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -147,7 +147,7 @@
 					}
 				).done(function(data) {
 				    console.log(data);
-				    var processArray = data;
+				    var goofert = data;
 				    console.log("Our post has returned data.");
 			  	}).fail(function() {
 					console.log("Our post has something wrong with it.");
