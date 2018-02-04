3237de62e35c8f1fa8f9ce016274e85b58d1c86c
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-04 15:56:44 -0700

    fdsa

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -147,7 +147,7 @@
 					}
 				).done(function(data) {
 				    console.log(data);
-				    var retrievedArr = this.data;
+				    var retrievedArr = data;
 				    console.log("Our post has returned data.");
 			  	}).fail(function() {
 					console.log("Our post has something wrong with it.");
