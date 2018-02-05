f71193091130ebc6a945a1d09a2652e5edc9921c
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-04 18:14:10 -0700

    sdsds

--- AmazonEC2/locationsByInterest.php
+++ AmazonEC2/locationsByInterest.php
@@ -71,7 +71,7 @@ try {
     }
 } catch(Exception $ex) {
     header('Content-Type: application/json');
-    echo json_encode(array( "error" => $ex) );
+    echo json_encode(array( "error" => $ex->getMessage()) );
     exit;
 }
 
