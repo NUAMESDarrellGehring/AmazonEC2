463cc5cf7826e2f34f8c95d7d5775394ca95a53a
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-04 18:04:24 -0700

    sds

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -71,7 +71,7 @@
 			<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
 		
 		
-			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+			<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 			
 			
