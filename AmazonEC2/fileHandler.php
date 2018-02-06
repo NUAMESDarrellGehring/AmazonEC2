bdbb67638b4511d2f58ccac887afd1cbf092598e
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-06 11:00:26 -0700

    afdsfdsa

--- AmazonEC2/fileHandler.php
+++ AmazonEC2/fileHandler.php
@@ -156,7 +156,7 @@
 
 				} ).on( 'xhr', function(e, settings, json) { //xhr is an event that occurs when an ajax action IS COMPLETED 
 				    console.log( 'Ajax event occurred. Returned data: ', json );
-				    console.log(json.data[0].city[0]);
+				    console.log(json.data[0].city);
 				} );
 				
 				console.log("end dynamicDataTable.");
