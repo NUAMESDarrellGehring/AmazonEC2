c44f0566f9ada6ea980f4825fd0f769dcf806cf6
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-18 09:01:44 -0700

    dsdf

--- AmazonEC2/PersonalLibrary/libraryHome.php
+++ AmazonEC2/PersonalLibrary/libraryHome.php
@@ -1,1 +1,16 @@
-<?php
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+	function loginVerify(){
+		console.log("Verifying log-in...");
+
+		$.ajax({
+
+		});
+	}
+</script>
+<html>
+	<form onsubmit="loginVerify" method="post" enctype="multipart/form-data">
+		<input type="submit" name="submitStatus" value="Login">
+	</form>
+</html>
