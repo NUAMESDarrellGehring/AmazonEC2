e09ea8d212df386429adb6d5665c823b1f94cf95
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-18 12:03:50 -0700

    ads

--- AmazonEC2/PersonalLibrary/registrationPage.php
+++ AmazonEC2/PersonalLibrary/registrationPage.php
@@ -1,3 +1,5 @@
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
 	function registerUser(){
 		console.log("Verifying registration...");
