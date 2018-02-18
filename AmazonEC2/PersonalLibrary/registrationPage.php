7d07b6906a4eef5231abc8dddffae87b9dac4b92
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-18 12:02:31 -0700

    sdffds

--- AmazonEC2/PersonalLibrary/registrationPage.php
+++ AmazonEC2/PersonalLibrary/registrationPage.php
@@ -1,1 +1,38 @@
-<?php
+<script>
+	function registerUser(){
+		console.log("Verifying registration...");
+
+		var password = $("input[name='password']").val();
+		var email = $("input[name='email']").val();
+
+		var logindata = new FormData();
+		logindata.append('email', password);
+		logindata.append('password', email);
+			
+		$.ajax({
+			url: "http://34.212.128.254/AmazonEC2/PersonalLibrary/createAccount.php",
+			data: logindata,
+			contentType: false,
+			processData: false,
+			type: 'POST',
+			success: function(data){
+				console.log(data);
+			},
+			fail: function(data){
+				console.log("Failure!");
+				console.log(data);
+			}
+			
+		});
+		return false;
+	}
+</script>
+
+<html>
+	<form onsubmit="return registerUser()" method="post" enctype="multipart/form-data">
+		User Email: <input type="text" name="email"><br>
+		User Password: <input type="password" name="password"><br>
+		<input type="submit" name="submitStatus" value="Make Account">
+	</form>
+	<br>
+</html>
