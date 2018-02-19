30830113e15699b2c911ba9bcca89a9612756355
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-19 11:04:40 -0700

    dscd

--- AmazonEC2/PersonalLibrary/Libraries.php
+++ AmazonEC2/PersonalLibrary/Libraries.php
@@ -3,7 +3,7 @@
 
     class Library extends BaseDBWrapper{
 
-        public function addbook(String $title, String $authorfirst, String $authorlast){
+        public function addbook(string $title, string $authorfirst, string $authorlast){
             return $this->getQueryResults("INSERT INTO books(title, authorfirst, authorlast, bookownerid) VALUES('".$title."','".$authorfirst."','".$authorlast."',".$_SESSION['USER_ID'].");");
         }
         
