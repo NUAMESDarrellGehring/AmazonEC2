3c10923a3184fe109f47cc9064f54a0be60965fd
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-19 10:54:45 -0700

    fdsfds

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -1,14 +1,7 @@
 <?php session_start();?>
 <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script>
-	var sessionActive = "<?= (isset($_SESSION['USER_ID']) ? $_SESSION['USER_ID'] : "") ?>";
-	if(sessionActive == "") {
-		window.location.href = 'http://34.212.128.254/AmazonEC2/PersonalLibrary/libraryHome.php';
-	}else{
-		console.log("Success - user id #"+sessionActive);
-	}
-</script>
+
 
 <script>
 	function addBook(){
