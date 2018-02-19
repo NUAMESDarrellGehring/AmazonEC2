d739978e27d1c0503ee8e5e64ca98116d1609624
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-19 10:33:42 -0700

    fdsd

--- AmazonEC2/PersonalLibrary/LibraryActions.php
+++ AmazonEC2/PersonalLibrary/LibraryActions.php
@@ -1,28 +1,33 @@
 <?php
-    require_once("Libraries.php");
-    session_start();
-
-    if(isset($_REQUEST['pageAction'])) {
-        $pageAction = strtolower($_REQUEST['pageAction']);
-    } else {
-        throw new Exception("Missing required request parameter: pageAction");
-    }
+    try{
+        require_once("Libraries.php");
+        session_start();
     
-    $library = new Library();
-    
-    switch($pageAction) {
-        case "createBook":
-            echo json_encode($library->addbook($_REQUEST['title'], $_REQUEST['authorfirst'], $_REQUEST['authorlast']));
+        if(isset($_REQUEST['pageAction'])) {
+            $pageAction = strtolower($_REQUEST['pageAction']);
+        } else {
+            throw new Exception("Missing required request parameter: pageAction");
+        }
+        
+        $library = new Library();
+        
+        switch($pageAction) {
+            case "createBook":
+                $output = $library->addbook($_REQUEST['title'], $_REQUEST['authorfirst'], $_REQUEST['authorlast']);
+            break;
             
-        break;
-        
-        case "deleteBook":
-        
-        break;
-        
-        case "getBooks":
+            case "deleteBook":
             
-        break;
+            break;
+            
+            case "getBooks":
+                
+            break;
+        }
+    } catch(Exception $ex) {
+        $output = json_encode(array("error" => $ex->getMessage()));
+    } finally {
+        header('Content-Type: application/json');
+        echo $output;
     }
-
 ?>
