db8c7c47c71ed46a7ae4f5808334aa29096b21c3
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-18 18:29:20 -0700

    dsadsa

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -1,7 +1,8 @@
 <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
-	if(<?php $_SESSION['id'] ?>=undefined){
+	var idDoesExist <?php $_SESSION['id'] ?>;
+	if(idDoesExist=undefined){
 		window.location.href = 'http://34.212.128.254/AmazonEC2/PersonalLibrary/libraryHome.php';
 	}
 </script>
