3ce772a58a93afdb867523f94fde61a269e3890b
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-19 12:18:39 -0700

    fdsfds

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -85,7 +85,7 @@
                 "url": "http://34.212.128.254/AmazonEC2/PersonalLibrary/LibraryActions.php", //had to change this address
                 "type": "POST",
                 "data": {
-                    'action' : 'getbooks',
+                    'pageAction' : 'getbooks',
                 }
             },
             
