459ebb1c5da766d21ffc01ca6dd45597209fba26
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-20 09:57:00 -0700

    dsadsa

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -68,7 +68,7 @@
     			processData: false,
     			type: 'POST',
     			success: function(data){
-    				if(data['error']!=undefined&&typeof(data['error']) != "nonexistant") {
+    				if(data['error']!=undefined&&typeof(data['error'])=="nonexistant") {
     					//We got an error back
     					alert("That book doesn't exist yet!");
     					console.log(data);
