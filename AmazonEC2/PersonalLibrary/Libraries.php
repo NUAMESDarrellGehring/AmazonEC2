186d8580e5412ae9d2a3d60e5d6f68a06182310f
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-19 20:12:13 -0700

    fdfds

--- AmazonEC2/PersonalLibrary/Libraries.php
+++ AmazonEC2/PersonalLibrary/Libraries.php
@@ -4,6 +4,7 @@
     class Library extends BaseDBWrapper{
 
         public function addbook($title, $authorfirst, $authorlast){
+            
             return $this->runQuery("INSERT INTO books(title, authorfirst, authorlast, bookownerid) VALUES('".$this->escapeString($title)."','".$this->escapeString($authorfirst)."','".$this->escapeString($authorlast)."',".$_SESSION['USER_ID'].");");
         }
         
@@ -12,7 +13,12 @@
         }
         
         public function getbooks($start, $length){
-            return $this->getQueryResultsLimited("SELECT * FROM books WHERE bookownerid = ".$_SESSION['USER_ID'], $start, $length);
+            $resultcountuntrimmed = sizeof($this->getQueryResults("SELECT * FROM books WHERE bookownerid = ".$_SESSION['USER_ID']));
+            $results = $this->getQueryResultsLimited("SELECT * FROM books WHERE bookownerid = ".$_SESSION['USER_ID'], $start, $length);
+            
+            $data = array("data" => $results, "recordsTotal" => $results, "recordsFiltered" => $rowCount);
+            
+            return $data;
         }
         
         public function getbookbyname(String $name){
