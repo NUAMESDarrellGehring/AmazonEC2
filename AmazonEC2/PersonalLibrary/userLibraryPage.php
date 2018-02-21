701b0bc5d05b27a8e498b9e45b0645365cd29629
Darrell Gehring <majesticwaffle17@gmail.com>
2018-02-21 10:13:58 -0700

    saasd

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -116,7 +116,7 @@
     					if(data['error'].indexOf("Duplicate") != -1) {
     						//Duplicate User
     						alert("You already entered this book!");
-    					} else if(data['error'].indexOf("invalidISBN")) {
+    					} else if(data['error'].indexOf("invalidISBN") != -1) {
         					alert("You've entered an invalid ISBN! It must be 13 digits and contain no characters.");
     					} else {
     						alert("Unknown Error:\n" + data['error']);
