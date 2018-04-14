4b7aaf2782f821a1d3ec8d9af55a6faafa786cfd
Darrell Gehring <majesticwaffle17@gmail.com>
2018-04-14 16:05:39 -0600

    dsadsa

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -285,7 +285,7 @@
             dynamicDataTable();
             $("#bookTable").on('click', 'tr', function() {
 				var data = $("#bookTable").DataTable().row(this).data;
-				alert(data);
+				console.log(data);
             });                
     	});
     	
