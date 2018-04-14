457077c9154352bfe6ed9c6e0dd4b6c3232d97a1
Darrell Gehring <majesticwaffle17@gmail.com>
2018-04-14 12:48:00 -0600

    fdslknm

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -257,7 +257,7 @@
 
     	/* Set the width of the side navigation to 0 */
     	function closeNav() {
-    	    document.getElementById("mySidenav").style.width = "0";
+    	    document.getElementById("Menu").style.width = "0";
     	    $("#Menu").css("width", "0");
     	}
     	
