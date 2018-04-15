b149e42ca8f4504d051b03f496647e0782a04a7c
Darrell Gehring <majesticwaffle17@gmail.com>
2018-04-15 15:51:28 -0600

    ddsads

--- AmazonEC2/PersonalLibrary/Libraries.php
+++ AmazonEC2/PersonalLibrary/Libraries.php
@@ -50,7 +50,7 @@
                 throw new Exception("Authors Last Name is a required");
             }
             
-            $existingBooks = $this->getbooks($title, $authorfirst, $authorlast, $isbn);
+            $existingBooks = $this->getbooks($title, $authorfirst, $authorlast, $isbn, null, null, null);
             if(count($existingBooks) > 0) {
                 throw new Exception("Another book already exists with these properties (".var_export($existingBooks, true).")");
             }
