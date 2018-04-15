3d65f2886d468ce6587881eb58302e003e69945f
Darrell Gehring <majesticwaffle17@gmail.com>
2018-04-14 18:18:30 -0600

    test

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -77,11 +77,20 @@
         }
     </style>
     
+    <!-- 
     <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.0/themes/cupertino/jquery-ui.css">
+    -->
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jqc-1.12.3/dt-1.10.16/b-1.5.1/sl-1.2.5/datatables.min.css"/>
 	
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/cupertino/jquery-ui.css">
+    
+    <!--
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" type="text/javascript"></script>
+    -->
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    
 	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jqc-1.12.3/dt-1.10.16/b-1.5.1/sl-1.2.5/datatables.min.js"></script>
    
     <script type="text/javascript">
@@ -281,7 +290,6 @@
             
             $("#bookTable tbody").on('click', 'tr', function() {
 				var data = $("#bookTable").DataTable().row(this).data();
-				alert($("#creatorForm"));
 				$("#creatorForm").dialog();
 				console.log(data);
             });                
