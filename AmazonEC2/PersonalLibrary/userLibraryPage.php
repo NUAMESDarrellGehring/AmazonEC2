448a4bad8ecb05df6e3c49e8ecf9c924abdfc90a
Darrell Gehring <majesticwaffle17@gmail.com>
2018-04-15 17:58:14 -0600

    addsads

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -464,7 +464,7 @@
 	   	<div id="mainPage">
 	    	<div class="addEntryButton" id="addEntry" onclick="addEntryDialog()">Add Book</div>
     	   	<div class="bottomLeft" id="tableDiv">
-    			<table id="bookTable" class="display" style="width: 400px">
+    			<table id="bookTable" class="display" style="width: 200px">
             		<thead>
                         <tr>
                             <th>Book</th>
