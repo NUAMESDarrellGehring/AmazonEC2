d18087f4e7f64005e45b59af311142ddbfd3edf1
Darrell Gehring <majesticwaffle17@gmail.com>
2018-04-15 19:25:01 -0600

    sdads

--- AmazonEC2/PersonalLibrary/userLibraryPage.php
+++ AmazonEC2/PersonalLibrary/userLibraryPage.php
@@ -133,7 +133,6 @@
     	var menuOpen=false;
 
 		function logout(){
-			<?php session_abort(); ?>
 			window.location.href = 'http://34.212.128.254/AmazonEC2/PersonalLibrary/libraryHome.php';			
 		}
     	
